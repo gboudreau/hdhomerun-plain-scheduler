e171f8ca77b3f755246921ee698a9b9688a03ad1
Guillaume Boudreau <guillaume@pommepause.com>
2017-08-19 09:23:56 -0400

    Improvement: allow Record blocks to NOT be separated with an empty line, in schedules file

--- classes/SchedulesParser.php
+++ classes/SchedulesParser.php
@@ -6,9 +6,10 @@ class SchedulesParser
     private $_schedules_text;
     private $_recordings = [];
 
+    private $_recording = NULL;
+
     public function __construct(string $schedules_file) {
         $this->_schedules_text = file_get_contents($schedules_file);
-
         $this->_parse();
     }
 
@@ -23,44 +24,46 @@ class SchedulesParser
         foreach (explode("\n", $this->_schedules_text) as $line) {
             $line_number++;
             $line = trim($line);
+
             if (empty($line)) {
                 // Empty line
-                if (isset($recording)) {
-                    // End of Recording block
-                    $recording->validate();
-                    $this->_recordings[] = $recording;
-                    unset($recording);
-                }
                 continue;
             }
+
             if ($line[0] == '#' || $line[0] == ';') {
                 // Comments
                 continue;
             }
 
             if (strtolower(trim($line)) == 'record') {
+                // Previous record bock ended (if any)
+                $this->_finishRecording();
+
                 // Recording block starts
-                $recording = new Recording();
+                $this->_recording = new Recording();
                 continue;
             }
 
-            if (empty($recording)) {
+            if (empty($this->_recording)) {
                 throw new \Exception("Fatal error: each recording schedule needs to start with the word 'Record' alone on a line. Found '$line' on line $line_number. Exiting.");
             }
 
-            $recording->addLine($line, $line_number);
+            $this->_recording->addLine($line, $line_number);
         }
 
-        if (isset($recording)) {
-            // End of Recording block at the end of the file
+        $this->_finishRecording();
+    }
+
+    private function _finishRecording() {
+        if (isset($this->_recording)) {
             try {
-                $recording->validate();
-                $this->_recordings[] = $recording;
+                $this->_recording->validate();
+                $this->_recordings[] = $this->_recording;
             } catch (\Exception $ex) {
                 // Ignore this recording, but log the error
                 _log($ex->getMessage());
             }
-            unset($recording);
+            unset($this->_recording);
         }
     }
 
